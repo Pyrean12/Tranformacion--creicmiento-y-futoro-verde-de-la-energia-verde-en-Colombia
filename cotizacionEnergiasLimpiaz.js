ee2e993e44b38768a1b24f0095ae92783d23e39c
Pyrean12 <hugo.villegas.v@gmail.com>
2025-10-18 23:23:01 -0500

    Add files via upload

--- cotizacionEnergiasLimpiaz.js
+++ cotizacionEnergiasLimpiaz.js
@@ -20,7 +20,8 @@ seleccion.addEventListener("change", function()
     }
 });
 
-function calularValor(){
+function calularValor()
+{
     let numero1 = Number(document.getElementById("cantidadPanels").value);
     let numero2 = Number(document.getElementById("precioUnidad").value);
     let resultado = 0;
@@ -31,3 +32,33 @@ function calularValor(){
     document.getElementById("resulPanel").innerText = resultado;
  
 } 
+const seleccion1 = document.getElementById("selector1");
+const precio1 = document.getElementById("precioUnidadEolica");
+
+seleccion1.addEventListener("change", function()
+{
+    let valor1=seleccion1.value;
+    if(valor1 === "eo1")
+    {
+        document.getElementById("precioUnidadEolica").value = "170000";
+    }
+    else if(valor1 ==="eo2")
+    {
+        document.getElementById("precioUnidadEolica").value = "180000";
+    }
+    else if(valor1 === "eo3")
+    {
+        document.getElementById("precioUnidadEolica").value = "190000";
+    }
+});
+
+l
+function calularValor1()
+{
+    let numeroEo1 = Number(document.getElementById("cantidadEolica").value);
+    let numeroEo2 = Number(document.getElementById("precioUnidadEolica").value);
+    let resultadoEo = 0;
+    resultadoEo= numeroEo1 * numeroEo2;
+    document.getElementById("resulEolico").innerText = resultadoEo;
+    
+}
